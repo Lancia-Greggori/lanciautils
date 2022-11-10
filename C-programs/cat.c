4e1307997fdb2dbdb6afda52e3238d96941331ae
Lancia <lanciagreggori@gmail.com>
2022-11-10 19:08:02 +0330

    Update

--- C-programs/cat.c
+++ C-programs/cat.c
@@ -5,13 +5,10 @@
 void read_file(FILE *fp)
 {
 	char character;
-
 	character = fgetc(fp);
-
 	while(character != EOF)
 	{
 		putchar(character);
-
 		character = fgetc(fp);
 	}
 }
@@ -19,40 +16,29 @@ void read_file(FILE *fp)
 int main(int argc, char *argv[])
 {
 	if(argc < 2) read_file(stdin);
-
 	else
 	{
 		FILE *fp;
-
 		for(int i = 1; i < argc; i++)
 		{
 			if(strcmp(argv[i], "-") == 0)
 			{
 				read_file(stdin);
-
 				continue;
 			}
-
 			else if(access(argv[i], F_OK) != 0)
 			{
-				fprintf(stderr, "Error: %s does not exist\n", argv[i]);
-
+				fprintf(stderr, "cat: [ERROR]: %s does not exist\n", argv[i]);
 				continue;
 			}
-
 			else if(access(argv[i], R_OK) != 0)
 			{
-				fprintf(stderr, "Error: %s is not readable\n", argv[i]);
-
+				fprintf(stderr, "cat: [ERROR]: %s is not readable\n", argv[i]);
 				continue;
 			}
-
 			fp = fopen(argv[i], "r");
-
 			read_file(fp);
 		}
-
 	}
-
 	return 0;
 }
