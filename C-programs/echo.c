69a718dcb8547d387c16c999f333912612637e0f
Lancia <lanciagreggori@gmail.com>
2022-12-11 17:51:39 +0330

    Update

--- C-programs/echo.c
+++ C-programs/echo.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	if (argc == 1) putchar('\n');
 	else if (argc == 2) puts(argv[1]);
-	else
-	{
-		for (int i = 1; i < argc; i++)
-		{
+	else {
+		for (int i = 1; i < argc; i++) {
 			char *p = argv[i];
-			for (int j = 0; p[j] != '\0'; j++)
-			{
+			for (int j = 0; p[j] != '\0'; j++) {
 				putchar(p[j]);
 			}
 			if (i != (argc-1)) putchar(' ');
