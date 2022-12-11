23a5a7bcc78df547c1a06748e55d11b8ca0275f1
Lancia <lanciagreggori@gmail.com>
2022-12-11 17:45:20 +0330

    Update

--- C-programs/cat.c
+++ C-programs/cat.c
@@ -2,41 +2,36 @@
 #include <unistd.h>
 #include <string.h>
 
-void read_file(FILE *fp)
-{
+void read_file(FILE *fp) {
 	char character;
-	character = fgetc(fp);
-	while(character != EOF)
-	{
+	character = fgetc (fp);
+	while (character != EOF) {
 		putchar(character);
 		character = fgetc(fp);
 	}
 }
 
-int main(int argc, char *argv[])
-{
-	if(argc < 2) read_file(stdin);
-	else
-	{
+int main(int argc, char *argv[]) {
+	if (argc < 2) read_file(stdin);
+	else {
 		FILE *fp;
-		for(int i = 1; i < argc; i++)
-		{
-			if(strcmp(argv[i], "-") == 0)
-			{
+		for (int i = 1; i < argc; i++) {
+			if (strcmp(argv[i], "-") == 0) {
 				read_file(stdin);
 				continue;
-			}
-			else if(access(argv[i], F_OK) != 0)
-			{
+			} else if (access(argv[i], F_OK) != 0) {
 				fprintf(stderr, "cat: [ERROR]: %s: no such file or directory\n", argv[i]);
 				continue;
-			}
-			else if(access(argv[i], R_OK) != 0)
-			{
+			} else if (access(argv[i], R_OK) != 0) {
 				fprintf(stderr, "cat: [ERROR]: %s: file not readable\n", argv[i]);
 				continue;
 			}
+
 			fp = fopen(argv[i], "r");
+			if (fp == 0) {
+				fprintf(stderr, "cat: [ERROR]: %s: fp is zero, skipping reading\n", argv[i]);
+				continue;
+			}
 			read_file(fp);
 		}
 	}
