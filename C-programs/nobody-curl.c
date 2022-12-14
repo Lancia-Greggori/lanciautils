847f48c7ea9c7461ec24de6e1dc21ea4816289a0
Lancia <lanciagreggori@gmail.com>
2022-12-14 21:00:05 +0330

    Update

--- C-programs/nobody-curl.c
+++ C-programs/nobody-curl.c
@@ -7,16 +7,15 @@ int main(int argc, char *argv[]) {
 	if (argc <= 1) return 1;
 	for (int i = 1; i < argc; i++) {
 		size = size + strlen(argv[i]);
-		if (i != argc-1) size++;
+		if (i != argc-1) size++; // account for spaces between arguments
 	}
 
-	char command[size];
+	char command[size+6]; // account for the string "curl " plus NULL terminator
 	strcpy(command, "curl ");
 	for (int i = 1; i < argc; i++) {
 		strcat(command, argv[i]);
 		if (i != argc-1) strcat(command, " ");
 	}
-
 
 	return 0;
 }
