59b53f0447353c724cc78967a28af50e34625d3f
Lancia <lanciagreggori@gmail.com>
2022-12-14 14:32:11 +0330

    Update

--- C-programs/nobody-curl.c
+++ C-programs/nobody-curl.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-	int ret = system("ls asdsdd");
-	return ret;
+	int size = 0;
+	if (argc <= 1) return 1;
+	for (int i = 1; i < argc; i++) {
+		size = size + strlen(argv[i]);
+		if (i != argc-1) size++;
+	}
+
+	char *command = calloc(argc-1, size);
+	command[0] = '\0'
+	for (int i = 1; i < argc; i++) {
+		strcat(command, argv[i]);
+		if (i != argc-1) strcat(command, " ");
+	}
+
+
+	return 0;
 }
