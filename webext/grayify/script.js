186292707c5616953e38ff89983c43c7abc33782
Lancia <lanciagreggori@gmail.com>
2023-09-22 23:32:42 +0200

    Update

--- webext/grayify/script.js
+++ webext/grayify/script.js
@@ -1,1 +1,10 @@
-document.querySelector('html').style.filter = "grayscale(100%)";
+if(/google\.com/.test(window.location.href)) {
+	var styles = '* { filter: grayscale(100%); }'
+	var styleSheet = document.createElement("style")
+	styleSheet.innerText = styles
+	document.head.appendChild(styleSheet)
+} else {
+	for (let i = 0; i < 100; i++) {
+		document.querySelector('html').style.filter = "grayscale(100%)";
+	}
+}
