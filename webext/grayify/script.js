18f4d0da0261004a48ef08eec0a2ff247f35aa24
Lancia <lanciagreggori@gmail.com>
2023-09-26 12:01:12 +0200

    Update

--- webext/grayify/script.js
+++ webext/grayify/script.js
@@ -1,10 +1,8 @@
-if(/google\.com/.test(window.location.href)) {
+if (/google\.com/.test(window.location.href)) {
 	var styles = '* { filter: grayscale(100%); }'
 	var styleSheet = document.createElement("style")
 	styleSheet.innerText = styles
 	document.head.appendChild(styleSheet)
 } else {
-	for (let i = 0; i < 100; i++) {
-		document.querySelector('html').style.filter = "grayscale(100%)";
-	}
+	document.querySelector('html').style.filter = "grayscale(100%)";
 }
