ce0e121f495a43a8331dbe044e88559229742370
Lancia <lanciagreggori@gmail.com>
2023-09-28 16:42:15 +0200

    Update

--- webext/grayify/disable.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("disableButton").addEventListener("click", myDisable);
-
-function myDisable() {
-	document.querySelector('html').style.setProperty("filter", "grayscale(0)", "important");
-}
