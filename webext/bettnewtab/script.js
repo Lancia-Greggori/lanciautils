d0f74288cbd5c19f51685bafa343933e3f30c805
Lancia <lanciagreggori@gmail.com>
2023-09-29 18:38:17 +0200

    Update

--- webext/bettnewtab/script.js
+++ webext/bettnewtab/script.js
@@ -1,6 +1,6 @@
 chrome.history.search( { text: '' }, function (histit) {
 	let str = "<h1>Even if you don't hit the ideal all the time, don't give up</h1>";
-		for (let i = 0; i < 10; i++) {
+		for (let i = 0; i < 20; i++) {
 			str = str + 
 				'<a style="color: gray" href="' + 
 				histit[i].url + '">' +
