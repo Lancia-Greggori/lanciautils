d4bab90971753d9ac90923c3f9525698b4615afe
Lancia <lanciagreggori@gmail.com>
2023-09-30 19:50:07 +0200

    Update

--- webext/bettnewtab/bkmrk.js
+++ webext/bettnewtab/bkmrk.js
@@ -1,17 +1,15 @@
 chrome.bookmarks.getRecent(100, 
 	function (bkmrkit) {
-		let str = '';
 		let title = '';
 		for (let i = bkmrkit.length - 1; i >= 0; i--) {
 			if (i > 0 &&
 				bkmrkit[i].title === bkmrkit[i-1].title) continue;
 			else if (!bkmrkit[i].title) title = bkmrkit[i].url;
 			else title = bkmrkit[i].title;
-			str = str + 
+			document.getElementById('bkmrk').innerHTML += 
 				'<a style="color: gray" href="' + 
 				bkmrkit[i].url + '">' + title +
 				'</a><p style="display: inline">&emsp;|&emsp;</p>';
 		}
-		document.getElementById('bkmrk').innerHTML = str;
 	}
 );
--- webext/bettnewtab/hist.js
+++ webext/bettnewtab/hist.js
@@ -1,17 +1,15 @@
 chrome.history.search( { text: '' }, 
 	function (histit) {
-		let str = '';
 		let title = '';
 		for (let i = 0; i < histit.length && i < 40; i++) {
 			if (i > 0 &&
 				histit[i].title === histit[i-1].title) continue;
 			else if (!histit[i].title) title = histit[i].url;
 			else title = histit[i].title;
-			str = str + 
+			document.getElementById('hist').innerHTML +=
 				'<a style="color: gray" href="' + 
 				histit[i].url + '">' + title +
 				'</a><p style="display: inline">&emsp;|&emsp;</p>';
 		}
-		document.getElementById('hist').innerHTML = str;
 	}
 );
