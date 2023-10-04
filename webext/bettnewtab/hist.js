70b0f86240ad3afa5fee5c6a7d87c45fa0e14eef
Lancia <lanciagreggori@gmail.com>
2023-10-04 18:39:48 +0200

    Update

--- webext/bettnewtab/hist.js
+++ webext/bettnewtab/hist.js
@@ -1,15 +1,18 @@
 chrome.history.search( { text: '' },
 	function (histit) {
 		let title = '';
-		for (let i = 0; i < histit.length && i < 10; i++) {
-			if (i > 0 &&
-				histit[i].title === histit[i-1].title) continue;
-			else if (!histit[i].title) title = histit[i].url;
-			else title = histit[i].title;
-			document.getElementById('hist').innerHTML +=
-				'<a style="color: gray" href="' +
-				histit[i].url + '">' + title +
-				'</a><br>';
+		for (let i = 0, max = 10; 
+			i < histit.length && i < max; i++) {
+				if (i > 0 &&
+					histit[i].title === histit[i-1].title) {
+						max++;
+						continue;
+				} else if (!histit[i].title) title = histit[i].url;
+				else title = histit[i].title;
+				document.getElementById('hist').innerHTML +=
+					'<a style="color: gray" href="' +
+					histit[i].url + '">' + title +
+					'</a><br>';
 		}
 	}
 );
