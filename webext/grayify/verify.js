9e507670b8c2955055f4f9823ccdf71d0df2e3c9
Lancia <lanciagreggori@gmail.com>
2023-10-22 21:01:21 +0200

    Update

--- webext/grayify/verify.js
+++ webext/grayify/verify.js
@@ -1,6 +1,6 @@
 function sleep(ms) {
 	    return new Promise(resolve => setTimeout(resolve, ms));
 }
-for( let i = 0; i < 10; i++, sleep(500) ) {
+for( let i = 0; i < 100; i++, sleep(200) ) {
 	document.querySelector('html').style.setProperty("filter", "grayscale(100%)", "important");
 }
