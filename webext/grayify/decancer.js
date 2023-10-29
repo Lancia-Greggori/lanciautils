32a6eb6d4b6d6c73a976d4cb154b45a929ac550c
Lancia <lanciagreggori@gmail.com>
2023-10-29 20:32:05 +0100

    Update

--- webext/grayify/decancer.js
+++ webext/grayify/decancer.js
@@ -1,6 +1,21 @@
 document.querySelector('html').style.setProperty('filter', 'grayscale(100%)', 'important');
 
-const tags = { a: 'a', p: 'p', u: 'u', div: 'div', input: 'input', span: 'span' };
+const tags = {
+	a: 'a',
+	p: 'p',
+	u: 'u',
+	h: 'h',
+	h1: 'h1',
+	h2: 'h2',
+	h3: 'h3',
+	h4: 'h4',
+	h5: 'h5',
+	h6: 'h6',
+	strong: 'strong',
+	div: 'div',
+	input: 'input',
+	span: 'span'
+};
 for (const tag in tags) {
 	var elmnt = document.getElementsByTagName(tag);
 	for (var i = 0; i < elmnt.length; i++) {
