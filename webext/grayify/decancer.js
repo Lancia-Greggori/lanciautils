ad5baa491f0d46c42626d29fa768f8bedecc7fbf
Lancia <lanciagreggori@gmail.com>
2023-11-01 15:38:40 +0100

    Update

--- webext/grayify/decancer.js
+++ webext/grayify/decancer.js
@@ -1,13 +1,25 @@
 document.querySelector('html').style.setProperty('filter', 'grayscale(100%)', 'important');
 
-var elmnt = document.querySelectorAll('*');
-for( var i = 0; i < elmnt.length; i++ ) {
-	if( elmnt[i].tagName === 'IMG' ) {
-		elmnt[i].style.filter = 'invert(100%)';
-		continue;
-	}
-	elmnt[i].style.setProperty('color', 'white', 'important');
-	elmnt[i].style.setProperty('background', 'black', 'important');
-	elmnt[i].style.setProperty('textDecoration', 'none', 'important');
-	elmnt[i].style.setProperty('borderBottom', 'none', 'important');
+const exclurls = [
+	'web.whatsapp.com',
+	'web.telegram.org',
+	'app.element.io',
+	'www.youtube.com'
+];
+
+if( ! exclurls.includes(window.location.hostname) ) {
+	let mstyle = `
+		*:not(img) {
+				color: white !important;
+				background: black !important;
+				text-decoration: none !important;
+				border-bottom: none !important
+		}
+
+		img { filter: invert(100%); }
+	`;
+
+	let mstsh = document.createElement('style');
+	mstsh.innerText = mstyle;
+	document.head.appendChild(mstsh);
 }
