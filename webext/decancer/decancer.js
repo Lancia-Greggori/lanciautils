cc74af0fab5de06c5885d8cc53c2e39f4040c50d
Lancia <lanciagreggori@gmail.com>
2023-11-10 22:24:44 +0100

    Update

--- webext/decancer/decancer.js
+++ webext/decancer/decancer.js
@@ -1,60 +1,72 @@
-document.querySelector("html").
-	style.setProperty("filter",
-	"grayscale(100%)", "important");
+if( window.location.hostname === "www.desmos.com" ) {
 
-const bg_black_exclurls = [
-	"web.whatsapp.com",
-	"web.telegram.org",
-	"app.element.io",
-	"www.google.com",
-	"www.youtube.com"
-];
-const font_exclurls = [
-	"math.libretexts.org",
-	"chem.libretexts.org",
-	"en.wikipedia.org"
-];
+	document.querySelector("html").
+		style.setProperty("filter",
+		"invert(1)", "important");
 
-var mstyle = `
-	*:not(img) {
-			color: white !important;
-	}
+} else {
 
-	* {
-			transition: none !important;
-			animation: none !important;
-			text-decoration: none !important;
-			border-bottom: none !important;
-	}
+	document.querySelector("html").
+		style.setProperty("filter",
+		"grayscale(1)", "important");
 
-	img { filter: invert(100%); }
-`;
+	const bg_black_excl_urls = [
+		"www.desmos.com",
+		"web.whatsapp.com",
+		"web.telegram.org",
+		"app.element.io",
+		"www.google.com",
+		"www.youtube.com"
+	];
+	const font_excl_urls = [
+		"math.libretexts.org",
+		"chem.libretexts.org",
+		"en.wikipedia.org"
+	];
 
-if( ! bg_black_exclurls.includes( window.location.hostname ) ) {
-	mstyle = mstyle + `
+	var mstyle = `
 		*:not(img) {
-			background: black !important;
+				color: white !important;
 		}
-	`;
-}
-if( ! font_exclurls.includes( window.location.hostname ) ) {
-	mstyle = mstyle + `
+
 		* {
-			font-family: sans-serif !important;
-			font-size: 17px !important;
-			font-weight: normal !important;
+				transition: none !important;
+				animation: none !important;
+				text-decoration: none !important;
+				text-shadow: none !important;
+				border-bottom: none !important;
 		}
+
+		img { filter: invert(1); }
 	`;
-}
 
-let mstsh = document.createElement("style");
-mstsh.innerText = mstyle;
-document.head.appendChild(mstsh);
-
-if( ! bg_black_exclurls.includes( window.location.hostname ) ) {
-	var elmnts = document.querySelectorAll("*:not(img)");
-	for( let i = 0; i < elmnts.length; i++ ) {
-		elmnts[i].style.setProperty("color", "white", "important");
-		elmnts[i].style.setProperty("background", "black", "important");
+	if( ! bg_black_excl_urls.includes( window.location.hostname ) ) {
+		mstyle = mstyle + `
+			*:not(img) {
+				background: black !important;
+			}
+		`;
 	}
+	if( ! font_excl_urls.includes( window.location.hostname ) ) {
+		mstyle = mstyle + `
+			* {
+				font-family: sans-serif !important;
+				font-size: 17px !important;
+				font-weight: normal !important;
+			}
+		`;
+	}
+
+	let mstsh = document.createElement("style");
+	mstsh.innerText = mstyle;
+	document.head.appendChild(mstsh);
+
+	if( ! bg_black_excl_urls.includes( window.location.hostname ) ) {
+		var elmnts = document.querySelectorAll("*:not(img)");
+		for( let i = 0; i < elmnts.length; i++ ) {
+			elmnts[i].style.setProperty("color", "white", "important");
+			elmnts[i].style.setProperty("background", "black", "important");
+		}
+	}
+
 }
